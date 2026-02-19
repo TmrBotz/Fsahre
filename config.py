ec00ad159e957061deee455b5185396bbb07504f
Tmrbotz <tmrbotz@gmail.com>
2026-02-19 18:19:29 +0530

    Update CHANNEL_ID and database connection details

--- config.py
+++ config.py
@@ -23,14 +23,14 @@ APP_ID = int(os.environ.get("APP_ID", "27696177")) #Your API ID from my.telegram
 API_HASH = os.environ.get("API_HASH", "0c44906a4feff3b947db76dfa7c57d88") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002591664525")) #Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002205504138")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "Tmr_Developer") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "6987799874")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://tmrbotz:1zW3leKja2QctZFx@cluster0.ltsyflo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://drozmarizabel991hull:Xh89XLrFTYOPgupl@cluster0.x8qoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Ana")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "60"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Tmr_Developer")
